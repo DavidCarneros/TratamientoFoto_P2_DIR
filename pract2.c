55f40aaa23f131900ea1457c2c3ac55964372282
DavidCarneros <David.Carneros1@alu.uclm.es>
2019-04-21 10:41:51 +0200

    Practica funcional

--- pract2.c
+++ pract2.c
@@ -6,7 +6,13 @@
 #include <X11/Xlib.h> 
 #include <assert.h>   
 #include <unistd.h>   
-#define NIL (0)       
+#define NIL (0)      
+
+#define NUM 4
+#define FOTO "foto.dat"
+#define FILA_IMAGEN 400
+#define INICIO 0
+#define FINAL 1
 
 /*Variables Globales */
 
@@ -57,16 +63,39 @@ void dibujaPunto(int x,int y, int r, int g, int b) {
 }
 
 
+int *RepartirTarea(int rank,int size){
+      static int reparto[2];
+      long totalFoto = FILA_IMAGEN * FILA_IMAGEN;      
+      int division = totalFoto / NUM;
+      reparto[INICIO] = division * rank;
+      reparto[FINAL] = division * (rank+1)-1;
+      if(rank==(size-1))
+      {
+            reparto[FINAL] = totalFoto;
+      }
+    
+
+      return reparto;
+}
+
+/* -------------- Declaracion de funciones --------------- */
+
+void tratarImagen(int rank,int *reparto, long bytesLeer, MPI_Comm commPadre,MPI_File imagen,char *flag);
+void aplicarFiltro(int x,int y,unsigned char *pixel,MPI_Comm commPadre, char *flag);
+void esperarPuntos(MPI_Comm commPadre);
+
 
 /* Programa principal */
 
 int main (int argc, char *argv[]) {
 
   int rank,size;
-  MPI_Comm commPadre;
+  MPI_Comm commPadre,intercomm;
   int tag;
+  int errcodes[NUM];
   MPI_Status status;
   int buf[5];
+  int i;
 
 
   MPI_Init(&argc, &argv);
@@ -80,22 +109,156 @@ int main (int argc, char *argv[]) {
 
 	/* Codigo del maestro */
 
+     
+      MPI_Comm_spawn("pract2",argv,NUM,MPI_INFO_NULL,0,MPI_COMM_WORLD,&intercomm,errcodes);
+      
+      printf("[Proceso PRINCIPAL] He creado %d procesos \n",NUM);
+
+      esperarPuntos(intercomm);
+
+      printf("[Proceso PRINCIPAL] Pulse cualquier tecla para finalizar la ejecucion...\n ");
+
+      getchar();
+
 	/*En algun momento dibujamos puntos en la ventana algo como
 	dibujaPunto(x,y,r,g,b);  */
 
         }
 
-  }
-
  	
-  else {
-    /* Codigo de todos los trabajadores */
-    /* El archivo sobre el que debemos trabajar es foto.dat */
+  else {      
 
+     /* Reparto */
+     int filaReparto = FILA_IMAGEN/NUM;
+     int reparto[2];
+     reparto[INICIO] = rank * filaReparto;
+     reparto[FINAL] = (rank+1) * filaReparto;
+     if(rank==size-1)
+     {
+           reparto[FINAL] = FILA_IMAGEN;
+     }
+     
+     long bytesLeer = filaReparto*FILA_IMAGEN*3*sizeof(unsigned char);
+      
+      MPI_File imagen;
+      MPI_File_open(MPI_COMM_WORLD, FOTO, MPI_MODE_RDONLY, MPI_INFO_NULL, &imagen);+
+
+      MPI_File_set_view(imagen, rank*bytesLeer, 
+            MPI_UNSIGNED_CHAR, MPI_UNSIGNED_CHAR, "native", MPI_INFO_NULL);
+
+      printf("[Proceso %d] y tengo inicio %d y final %d\n",rank,reparto[INICIO],reparto[FINAL]);
+      tratarImagen(rank,reparto,bytesLeer,commPadre,imagen,argv[2]);
 
   }
 
   MPI_Finalize();
 
 }
+/**
+ * 
+ * Funcion encargada de leer los pixeles del fichero foto.dat y llamar a la funcion aplicarfiltro por cada pixel
+ * de la imagen
+ * 
+*/
+void tratarImagen(int rank,int *reparto, long bytesLeer, MPI_Comm commPadre,MPI_File imagen,char *flag)
+{
 
+      int i,j;
+      unsigned char pixel[3];
+      int mensaje[5];
+      MPI_Status status;
+
+      /* Recorremos cada fila que nos ha tocado */
+      for(i=reparto[INICIO];i<reparto[FINAL];i++)
+      {
+            /* Recorremos cada columna */
+            for(j=0;j<FILA_IMAGEN;j++)
+            {
+                  MPI_File_read(imagen, pixel, 3, MPI_UNSIGNED_CHAR, &status);
+                  aplicarFiltro(j,i,pixel,commPadre,flag);
+            } 
+      }
+      MPI_File_close(&imagen);
+
+}
+/**
+ * 
+ * Funcion encargada de aplicar el filtro al pixel leido y mandarlo al proceso principal para que lo 
+ * dibuje en pantalla.
+ * 
+*/
+void aplicarFiltro(int x,int y,unsigned char *pixel,MPI_Comm commPadre, char *flag)
+{
+      int mensaje[5];
+      MPI_Status status;
+      mensaje[0]=x;
+      mensaje[1]=y;
+      int i=0;
+
+      switch (*flag)
+      {
+            case 'R':                 
+                  mensaje[2] = (int)pixel[0]; 
+                  mensaje[3] = 0;
+                  mensaje[4] = 0;
+                  break;
+            case 'G':
+                  mensaje[2] = 0;
+                  mensaje[3] = (int)pixel[1];
+                  mensaje[4] = 0;
+                  break;
+            case 'B':
+                  mensaje[2] = 0;
+                  mensaje[3] = 0;
+                  mensaje[4] = (int)pixel[2];
+                  break;
+            case 'W':
+                  mensaje[2] = (int)(pixel[0]*0.2986)+(int)(pixel[1]*0.5870)+(int)(pixel[2]*0.1140);
+                  mensaje[3] = (int)(pixel[0]*0.2986)+(int)(pixel[1]*0.5870)+(int)(pixel[2]*0.1140);
+                  mensaje[4] = (int)(pixel[0]*0.2986)+(int)(pixel[1]*0.5870)+(int)(pixel[2]*0.1140);
+                  break;
+            case 'N':
+                  mensaje[2] = 255-(int)pixel[0];
+                  mensaje[3] = 255-(int)pixel[1];
+                  mensaje[4] = 255-(int)pixel[2];
+                  break;
+            default:
+                  mensaje[2] = (int)pixel[0];
+                  mensaje[3] = (int)pixel[1];
+                  mensaje[4] = (int)pixel[2];
+                  break;
+      }
+      
+      for(i=2;i<=4;i++)
+      {
+            if(mensaje[i]>255)
+            {
+                 mensaje[i] = 255; 
+            }
+            else if (mensaje[i]<0)
+            {
+                  mensaje[i] = 0;
+            }
+      }
+
+      MPI_Bsend(&mensaje,5,MPI_INT,0,1,commPadre);
+
+}
+/**
+ * 
+ * Funcion que es llamada por el proceso principal, se encarga de recibir todos los puntos de los 
+ * demas procesos y llamar a la funcion dibujaPunto para pintarlo
+ * 
+*/
+void esperarPuntos(MPI_Comm commPadre)
+{
+      int buff[5];
+      int i;
+      long size_imagen = FILA_IMAGEN*FILA_IMAGEN;
+      MPI_Status status;
+      for(i=0;i<size_imagen;i++)
+      {
+            MPI_Recv(&buff,5,MPI_INT,MPI_ANY_SOURCE,1,commPadre,&status);
+            dibujaPunto(buff[0],buff[1],buff[2],buff[3],buff[4]);
+      }
+}
